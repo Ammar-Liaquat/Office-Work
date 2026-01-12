3e4ed04b6cdc64d60ab0777d0e9df55d60e6591f
Ammar-Liaquat <Ammarliaquatali7@gmail.com>
2026-01-12 16:58:27 +0500

    Delete Many Ids

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,99 +1,89 @@
-const User = require("../models/userModels")
+const User = require("../models/userModels");
 
 const createuser = async (req, res) => {
+  try {
+    const { name, email, password, gender } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+      gender,
+    });
+    res.status(201).json({
+      message: "user successfully created",
+      code: 201,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
 
-    try {
-
-        const { name,email,password,gender } = req.body
-        const user = await User.create({
-            name,
-            email,
-            password,
-            gender,
-            
-        })
-        res.status(201).json({
-            message: "user successfully created",
-            code: 201,
-            data: user
-
-        })
-    }
-    catch (error) {
-        console.log(error);
-
-        res.status(404).json({
-            message: "user not created",
-            code: 404,
-            error: error.message
-        })
-    }
-}
+    res.status(404).json({
+      message: "user not created",
+      code: 404,
+      error: error.message,
+    });
+  }
+};
 
 const getuser = async (req, res) => {
+  try {
+    const user = await User.find();
+    // if (!user) return res.status(404).json({ message: "User not found", code: 404 })
 
-    try {
-       
-        const user = await User.find()
-        // if (!user) return res.status(404).json({ message: "User not found", code: 404 })
+    res.status(200).json({
+      code: 200,
+      message: "user get fetched",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "user not fetched",
+      error: error.message,
+    });
+  }
+};
 
-        res.status(200).json({
-            code: 200,
-            message: "user get fetched",
-            data: user
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "user not fetched",
-            error: error.message
-        })
+const patchuser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json({
+      message: "user Updated succesfully",
+      code: 200,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+      code: 500,
+    });
+  }
+};
+
+const deluser = async (req, res) => {
+  try {
+    const { ids } = req.body;
+    const user = await User.deleteMany({_id:ids});
+    res.status(200).json({
+      message: "user deleted succesfully",
+      code: 200,
+      data: user,
+    });
+    for (let id in ids) {
     }
-
-}
-
-const patchuser = async (req,res)=>{
-
-    try {
-        const user = await User.findByIdAndUpdate({_id: req.params.id},{$set:req.body})
-        res.status(200).json({
-            message:"user Updated succesfully",
-            code:200,
-            data:user
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            message:"internal server error",
-            code:500
-        })
-    }
-
-}
-
-const deluser = async (req,res)=>{
-
-    try {
-        const user = await User.findByIdAndDelete({_id: req.params.id})
-        res.status(200).json({
-            message:"user Updated succesfully",
-            code:200,
-            data:user
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            message:"internal server error",
-            code:500
-        })
-    }
-
-}
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+      code: 500,
+    });
+  }
+};
 
 module.exports = {
-    createuser,
-    getuser,
-    patchuser,
-    deluser
-
-}
+  createuser,
+  getuser,
+  patchuser,
+  deluser,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const { createuser , getuser, patchuser, deluser} = require("../controllers/user
 routes.post("/",createuser)
 routes.get("/",getuser)
 routes.patch("/:id",patchuser)
-routes.delete("/:id",deluser)
+routes.delete("/",deluser)
 
 
 
